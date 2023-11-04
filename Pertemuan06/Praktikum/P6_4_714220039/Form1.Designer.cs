eae9b947fad2ad00251bbfee6ce1c7a872cb28bb
agung6544 <agung.nainggolan8008@gmail.com>
2023-11-04 18:37:36 +0700

    Upload_P6_4

---
+++ Pertemuan06/Praktikum/P6_4_714220039/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace P6_4_714220039
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_umur = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmb_JenisKelamin = new System.Windows.Forms.ComboBox();
+            this.btnMasuk = new System.Windows.Forms.Button();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(225, 108);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(160, 22);
+            this.tbNama.TabIndex = 0;
+            this.tbNama.Leave += new System.EventHandler(this.tbNama_Leave);
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(225, 179);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(160, 22);
+            this.tbEmail.TabIndex = 2;
+            this.tbEmail.Leave += new System.EventHandler(this.tbEmail_Leave);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Viner Hand ITC", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblName.Location = new System.Drawing.Point(110, 108);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(65, 29);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Nama";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(110, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Viner Hand ITC", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(42, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Jenis Kelamin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Viner Hand ITC", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(114, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Umur";
+            // 
+            // tb_umur
+            // 
+            this.tb_umur.Location = new System.Drawing.Point(225, 220);
+            this.tb_umur.Name = "tb_umur";
+            this.tb_umur.Size = new System.Drawing.Size(38, 22);
+            this.tb_umur.TabIndex = 3;
+            this.tb_umur.Leave += new System.EventHandler(this.tb_umur_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Viner Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(81, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(369, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "FORM MASUK WAHANA HORROR";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(286, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "tahun";
+            // 
+            // cmb_JenisKelamin
+            // 
+            this.cmb_JenisKelamin.FormattingEnabled = true;
+            this.cmb_JenisKelamin.Items.AddRange(new object[] {
+            "Laki-laki",
+            "Perempuan"});
+            this.cmb_JenisKelamin.Location = new System.Drawing.Point(225, 143);
+            this.cmb_JenisKelamin.Name = "cmb_JenisKelamin";
+            this.cmb_JenisKelamin.Size = new System.Drawing.Size(160, 24);
+            this.cmb_JenisKelamin.TabIndex = 10;
+            this.cmb_JenisKelamin.Leave += new System.EventHandler(this.cmb_JenisKelamin_Leave);
+            // 
+            // btnMasuk
+            // 
+            this.btnMasuk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnMasuk.Font = new System.Drawing.Font("Viner Hand ITC", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMasuk.Location = new System.Drawing.Point(134, 294);
+            this.btnMasuk.Name = "btnMasuk";
+            this.btnMasuk.Size = new System.Drawing.Size(82, 31);
+            this.btnMasuk.TabIndex = 11;
+            this.btnMasuk.Text = "Masuk";
+            this.btnMasuk.UseVisualStyleBackColor = false;
+            this.btnMasuk.Click += new System.EventHandler(this.btnMasuk_Click);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnKeluar.Font = new System.Drawing.Font("Viner Hand ITC", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeluar.Location = new System.Drawing.Point(333, 294);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(79, 31);
+            this.btnKeluar.TabIndex = 12;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = false;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click);
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(527, 362);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnMasuk);
+            this.Controls.Add(this.cmb_JenisKelamin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.tb_umur);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbNama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_umur;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmb_JenisKelamin;
+        private System.Windows.Forms.Button btnMasuk;
+        private System.Windows.Forms.Button btnKeluar;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+    }
+}
+
---
+++ Pertemuan06/Praktikum/P6_4_714220039/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_4_714220039
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbNama_Leave(object sender, EventArgs e)
+        {
+            if (tbNama.Text == "")
+            {
+                epWarning.SetError(tbNama, "");
+                epWrong.SetError(tbNama, "Tidak Boleh Kosong");
+                epCorrect.SetError(tbNama, "");
+            }
+            else if ((tbNama.Text).All(Char.IsUpper))
+            {
+                epWarning.SetError(tbNama, "");
+                epWrong.SetError(tbNama, "");
+                epCorrect.SetError(tbNama, "Betul");
+            }
+            else
+            {
+                epWarning.SetError(tbNama, "Inputan hanya boleh huruf besar!");
+                epWrong.SetError(tbNama, "");
+                epCorrect.SetError(tbNama, "");
+            }
+        }
+
+        private void tbEmail_Leave(object sender, EventArgs e)
+        {
+            if (tbEmail.Text == "")
+            {
+                epWarning.SetError(tbEmail, "");
+                epWrong.SetError(tbEmail, "Tidak Boleh Kosong");
+                epCorrect.SetError(tbEmail, "");
+            }
+            else if (tbEmail.Text.All(c => Char.IsLower(c) || Char.IsDigit(c) || c == '@' || c == '.') && Regex.IsMatch(tbEmail.Text, @"^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                epWarning.SetError(tbEmail, "");
+                epWrong.SetError(tbEmail, "");
+                epCorrect.SetError(tbEmail, "Betul");
+            }
+            else
+            {
+                epWarning.SetError(tbEmail, "Format email salah harus huruf kecil!\nContoh: a@b.c");
+                epWrong.SetError(tbEmail, "");
+                epCorrect.SetError(tbEmail, "");
+            }
+        }
+
+        private void tb_umur_Leave(object sender, EventArgs e)
+        {
+            if (tb_umur.Text == "")
+            {
+                epWarning.SetError(tb_umur, "");
+                epWrong.SetError(tb_umur, "Tidak Boleh Kosong");
+                epCorrect.SetError(tb_umur, "");
+            }
+            else if (tb_umur.Text.Length != 2 || !tb_umur.Text.All(Char.IsDigit))
+            {
+                epWarning.SetError(tb_umur, "Input hanya 2 digit angka");
+                epWrong.SetError(tb_umur, "");
+                epCorrect.SetError(tb_umur, "");
+            }
+            else
+            {
+                epWarning.SetError(tb_umur, "");
+                epWrong.SetError(tb_umur, "");
+                epCorrect.SetError(tb_umur, "Betul");
+            }
+        }
+
+        private void btnMasuk_Click(object sender, EventArgs e)
+            
+        {
+            if (string.IsNullOrWhiteSpace(tbNama.Text) || !tbNama.Text.All(Char.IsUpper))
+            {
+                MessageBox.Show("Nama harus diisi dan hanya boleh huruf besar.", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(cmb_JenisKelamin.Text) && cmb_JenisKelamin.SelectedIndex != -1)
+            {
+                MessageBox.Show("Jenis Kelamin harus dipilih.", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(tbEmail.Text) || !tbEmail.Text.All(c => Char.IsLower(c) || Char.IsDigit(c) || c == '@' || c == '.') || !Regex.IsMatch(tbEmail.Text, @"^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                MessageBox.Show("Email harus diisi dengan format yang benar.", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(tb_umur.Text) || tb_umur.Text.Length != 2 || !tb_umur.Text.All(Char.IsDigit))
+            {
+                MessageBox.Show("Umur harus diisi dengan 2 digit angka.", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int umur = int.Parse(tb_umur.Text);
+            if (umur >= 17)
+            {
+                string output = $"Nama: {tbNama.Text}\nJenis Kelamin: {cmb_JenisKelamin.SelectedItem}\nEmail: {tbEmail.Text}\nUmur: {tb_umur.Text} tahun\n\nInformasi Selanjutnya Lewat Email!";
+                MessageBox.Show(output, "Berhasi Mendaftar!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Anda Tidak Boleh Mendaftar, Umur anda belum 17 tahun atau lebih", "MAAF!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cmb_JenisKelamin_Leave(object sender, EventArgs e)
+        {
+            if (cmb_JenisKelamin.Text == "")
+            {
+                epWarning.SetError(cmb_JenisKelamin, "");
+                epWrong.SetError(cmb_JenisKelamin, "Tidak Boleh Kosong");
+                epCorrect.SetError(cmb_JenisKelamin, "");
+            }
+            else if (cmb_JenisKelamin.SelectedIndex != -1)
+            {
+                epWarning.SetError(cmb_JenisKelamin, "");
+                epWrong.SetError(cmb_JenisKelamin, "");
+                epCorrect.SetError(cmb_JenisKelamin, "Betul");
+            }
+            else
+            {
+                epWarning.SetError(cmb_JenisKelamin, "Pilih Jenis Kelamin!");
+                epWrong.SetError(cmb_JenisKelamin, "");
+                epCorrect.SetError(cmb_JenisKelamin, "");
+            }
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
