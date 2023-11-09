4fcfbed3a2d7046aa8af066d23addb85e498e0a4
agung6544 <agung.nainggolan8008@gmail.com>
2023-11-09 21:51:15 +0700

    Upload_P7

---
+++ Pertemuan07/Praktikum/P7_1_714220039/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace P7_1_714220039
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbProdi = new System.Windows.Forms.TextBox();
+            this.tbKelas = new System.Windows.Forms.TextBox();
+            this.btnCek = new System.Windows.Forms.Button();
+            this.btnTutupForm = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbSenin = new System.Windows.Forms.RadioButton();
+            this.rbMinggu = new System.Windows.Forms.RadioButton();
+            this.cbKuliah = new System.Windows.Forms.CheckBox();
+            this.cbTravelling = new System.Windows.Forms.CheckBox();
+            this.cbTidur = new System.Windows.Forms.CheckBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnResetForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(139, 44);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(267, 22);
+            this.tbNama.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prodi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Kelas";
+            // 
+            // tbProdi
+            // 
+            this.tbProdi.Location = new System.Drawing.Point(139, 95);
+            this.tbProdi.Name = "tbProdi";
+            this.tbProdi.Size = new System.Drawing.Size(267, 22);
+            this.tbProdi.TabIndex = 4;
+            // 
+            // tbKelas
+            // 
+            this.tbKelas.Location = new System.Drawing.Point(139, 146);
+            this.tbKelas.Name = "tbKelas";
+            this.tbKelas.Size = new System.Drawing.Size(267, 22);
+            this.tbKelas.TabIndex = 5;
+            // 
+            // btnCek
+            // 
+            this.btnCek.Location = new System.Drawing.Point(139, 235);
+            this.btnCek.Name = "btnCek";
+            this.btnCek.Size = new System.Drawing.Size(75, 33);
+            this.btnCek.TabIndex = 6;
+            this.btnCek.Text = "Cek";
+            this.btnCek.UseVisualStyleBackColor = true;
+            this.btnCek.Click += new System.EventHandler(this.btnCek_Click);
+            // 
+            // btnTutupForm
+            // 
+            this.btnTutupForm.Location = new System.Drawing.Point(309, 235);
+            this.btnTutupForm.Name = "btnTutupForm";
+            this.btnTutupForm.Size = new System.Drawing.Size(115, 33);
+            this.btnTutupForm.TabIndex = 7;
+            this.btnTutupForm.Text = "Tutup Form";
+            this.btnTutupForm.UseVisualStyleBackColor = true;
+            this.btnTutupForm.Click += new System.EventHandler(this.btnTutupForm_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(195, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "LIST KEGIATAN";
+            // 
+            // rbSenin
+            // 
+            this.rbSenin.AutoSize = true;
+            this.rbSenin.Location = new System.Drawing.Point(39, 332);
+            this.rbSenin.Name = "rbSenin";
+            this.rbSenin.Size = new System.Drawing.Size(62, 20);
+            this.rbSenin.TabIndex = 9;
+            this.rbSenin.TabStop = true;
+            this.rbSenin.Text = "Senin";
+            this.rbSenin.UseVisualStyleBackColor = true;
+            this.rbSenin.CheckedChanged += new System.EventHandler(this.rbSenin_CheckedChanged);
+            // 
+            // rbMinggu
+            // 
+            this.rbMinggu.AutoSize = true;
+            this.rbMinggu.Location = new System.Drawing.Point(39, 358);
+            this.rbMinggu.Name = "rbMinggu";
+            this.rbMinggu.Size = new System.Drawing.Size(72, 20);
+            this.rbMinggu.TabIndex = 10;
+            this.rbMinggu.TabStop = true;
+            this.rbMinggu.Text = "Minggu";
+            this.rbMinggu.UseVisualStyleBackColor = true;
+            this.rbMinggu.CheckedChanged += new System.EventHandler(this.rbMinggu_CheckedChanged);
+            // 
+            // cbKuliah
+            // 
+            this.cbKuliah.AutoSize = true;
+            this.cbKuliah.Location = new System.Drawing.Point(39, 412);
+            this.cbKuliah.Name = "cbKuliah";
+            this.cbKuliah.Size = new System.Drawing.Size(65, 20);
+            this.cbKuliah.TabIndex = 11;
+            this.cbKuliah.Text = "Kuliah";
+            this.cbKuliah.UseVisualStyleBackColor = true;
+            // 
+            // cbTravelling
+            // 
+            this.cbTravelling.AutoSize = true;
+            this.cbTravelling.Location = new System.Drawing.Point(39, 451);
+            this.cbTravelling.Name = "cbTravelling";
+            this.cbTravelling.Size = new System.Drawing.Size(89, 20);
+            this.cbTravelling.TabIndex = 12;
+            this.cbTravelling.Text = "Travelling";
+            this.cbTravelling.UseVisualStyleBackColor = true;
+            // 
+            // cbTidur
+            // 
+            this.cbTidur.AutoSize = true;
+            this.cbTidur.Location = new System.Drawing.Point(39, 489);
+            this.cbTidur.Name = "cbTidur";
+            this.cbTidur.Size = new System.Drawing.Size(60, 20);
+            this.cbTidur.TabIndex = 13;
+            this.cbTidur.Text = "Tidur";
+            this.cbTidur.UseVisualStyleBackColor = true;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(139, 530);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 33);
+            this.btnPrint.TabIndex = 14;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnResetForm
+            // 
+            this.btnResetForm.Location = new System.Drawing.Point(309, 530);
+            this.btnResetForm.Name = "btnResetForm";
+            this.btnResetForm.Size = new System.Drawing.Size(115, 33);
+            this.btnResetForm.TabIndex = 15;
+            this.btnResetForm.Text = "Reset Form";
+            this.btnResetForm.UseVisualStyleBackColor = true;
+            this.btnResetForm.Click += new System.EventHandler(this.btnResetForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 578);
+            this.Controls.Add(this.btnResetForm);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.cbTidur);
+            this.Controls.Add(this.cbTravelling);
+            this.Controls.Add(this.cbKuliah);
+            this.Controls.Add(this.rbMinggu);
+            this.Controls.Add(this.rbSenin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnTutupForm);
+            this.Controls.Add(this.btnCek);
+            this.Controls.Add(this.tbKelas);
+            this.Controls.Add(this.tbProdi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbProdi;
+        private System.Windows.Forms.TextBox tbKelas;
+        private System.Windows.Forms.Button btnCek;
+        private System.Windows.Forms.Button btnTutupForm;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rbSenin;
+        private System.Windows.Forms.RadioButton rbMinggu;
+        private System.Windows.Forms.CheckBox cbKuliah;
+        private System.Windows.Forms.CheckBox cbTravelling;
+        private System.Windows.Forms.CheckBox cbTidur;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnResetForm;
+    }
+}
+
